b879b05c336430b2793c33365067763e324ee9ed
Valentin <vtin88@yahoo.com>
2018-10-18 16:30:17 +0300

    Add scrollView fro gallery

--- Countries/Views/Controllers/CountriesDetails/CountryDetailsVC.swift
+++ Countries/Views/Controllers/CountriesDetails/CountryDetailsVC.swift
@@ -10,9 +10,16 @@ import UIKit
 
 
 class CountryDetailsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ 
 
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var scrollViewGallery: UIScrollView!
+    
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+    let maxHeaderHeight: CGFloat = 240;
+    let minHeaderHeight: CGFloat = 44;
+    var previousScrollOffset: CGFloat = 0;
+    
     
     var titleText: String = "default"
     var capitalName: String = "Moscow"
@@ -22,25 +29,39 @@ class CountryDetailsVC: UIViewController, UITableViewDataSource, UITableViewDele
     var descriptionFull: String = "Foo bar"
 
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 
+        //make navigation bar transparent
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         
-
         tableView.delegate = self
         tableView.dataSource = self
         tableView.tableFooterView = UIView(frame: .zero)
+        tableView.contentInsetAdjustmentBehavior = .never
         
+        //Fake data for gallery
+        var images = [UIImage]()
+        images.append(UIImage(named: "i-flag")!)
+        images.append(UIImage(named: "i-flag")!)
+        images.append(UIImage(named: "i-flag")!)
+        images.append(UIImage(named: "i-flag")!)
+        
+        setGalleryImages(&images)
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.headerHeightConstraint.constant = maxHeaderHeight
     }
     
     // MARK: - TableView section
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-   
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,10 +70,8 @@ class CountryDetailsVC: UIViewController, UITableViewDataSource, UITableViewDele
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //let cell = tableView.dequeueReusableCell(withIdentifier: "DetailInfoCell", for: indexPath) as! DetailInfoCell
         let cell = tableView.dequeueReusableCell(withIdentifier: "DetailInfoCell", for: indexPath) as! DetailInfoCell
-      
-        
+
         if indexPath.row == 0 {
             
             let countryNameCell = tableView.dequeueReusableCell(withIdentifier: "CountryDetailNameCell") as! CountryDetailNameCell
@@ -67,9 +86,7 @@ class CountryDetailsVC: UIViewController, UITableViewDataSource, UITableViewDele
             cell.title?.text = "Capital"
             cell.dataLabel?.text = capitalName
         }
-
-        
-        
+  
         if indexPath.row == 2 {
             
             cell.icon?.image = UIImage(named: "i-face")
@@ -91,9 +108,25 @@ class CountryDetailsVC: UIViewController, UITableViewDataSource, UITableViewDele
            return cell
         }
         
-        
-        
         return cell
     }
     
+    fileprivate func setGalleryImages(_ images: inout [UIImage]) {
+        for i in 0..<images.count {
+            let imageView = UIImageView()
+            let x = scrollViewGallery.frame.size.width * CGFloat(i)
+            imageView.frame = CGRect(x: x, y: 0, width: scrollViewGallery.frame.width, height: scrollViewGallery.frame.height)
+            imageView.contentMode = .scaleToFill
+            imageView.image = images[i]
+            
+            scrollViewGallery.bounces = false
+            scrollViewGallery.contentSize.width = scrollViewGallery.frame.size.width * CGFloat(i + 1)
+            scrollViewGallery.addSubview(imageView)
+        }
+    }
 }
+
+
+    
+
+
